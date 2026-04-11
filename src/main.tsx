59436d695744443a10d493b072043f2ddf43a9c4
Yidan Mao <jojordanbless@gmail.com>
2026-04-11 10:57:04 +0900

    Fix Cloudflare asset reload for homepage

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+;(window as Window & { __JLPT_SIMULATOR_BUILD__?: string }).__JLPT_SIMULATOR_BUILD__ = '2026-04-11-mime-fix'
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
